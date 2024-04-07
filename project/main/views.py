5142a41c2d91aa2c6f9e3d5dd38e8a2a109d105f
chaehyeo-n <parkche03@naver.com>
2024-04-07 21:26:30 +0900

    2주차 과제

--- project/main/views.py
+++ project/main/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def mainpage(request):
+    return render(request, 'main/mainpage.html')
+
+def baskinrobbins(request):
+    return render(request, 'main/baskinrobbins.html')
